21cdb2d8fd6b0b1e2c695153d8076ecbc380a8b0
unknown <abdallwario96@gmail.com>
2022-01-30 18:03:42 +0300

    url configs

--- backend/settings.py
+++ backend/settings.py
@@ -35,7 +35,7 @@ SECRET_KEY = 'django-insecure-xtiw7gh7u^ege*wdy_wd1d(#c13)s7ah_k+bg=x%mpnm+kyc*t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['blogisiolo.herokuapp.com','isiolotimes.com','localhost','127.0.0.1']
 
 
 # Application definition
--- backend/urls.py
+++ backend/urls.py
@@ -17,16 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView
 from api import views
-from django.conf.urls.static import static
-from django.conf import settings
-
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('',admin.site.urls),
     path('admin/', admin.site.urls),
     path('api/',include('api.urls')),
     path('summernote/', include('django_summernote.urls')),
     path('',TemplateView.as_view(template_name='index.html'))
 
-]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
