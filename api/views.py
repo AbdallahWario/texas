18b03ab098f6ddbc1e8e8c3e72109c066fbc2382
unknown <abdallwario96@gmail.com>
2022-02-12 09:02:18 +0300

    db configs

--- api/views.py
+++ api/views.py
@@ -18,16 +18,16 @@ import os
 
 # Create your views here.
 
-class Assets(View):
+# class Assets(View):
 
-    def get(self, _request, filename):
-        path = os.path.join(os.path.dirname(__file__), 'static', filename)
+#     def get(self, _request, filename):
+#         path = os.path.join(os.path.dirname(__file__), 'static', filename)
 
-        if os.path.isfile(path):
-            with open(path, 'rb') as file:
-                return HttpResponse(file.read(), content_type='application/javascript')
-        else:
-            return HttpResponseNotFound()
+#         if os.path.isfile(path):
+#             with open(path, 'rb') as file:
+#                 return HttpResponse(file.read(), content_type='application/javascript')
+#         else:
+#             return HttpResponseNotFound()
 
 def index(request):
     return render(request, "index.html")
